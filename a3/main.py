5c02041532ca44361d0a7dda140c23771a09d114
toan <bachmatoan2910@gmail.com>
2024-10-21 16:20:53 -0400

    Add comment to a3

--- a3/main.py
+++ a3/main.py
@@ -1,114 +1,123 @@
 from collections import defaultdict
 from abc import ABC, abstractmethod
 
+# ---------- Iterator Interfaces and Implementations ----------
 
-'''
-    Iterator declaration area start from here
-'''
 class SocialNetwork(ABC):
+    """Abstract base class representing a social network."""
     @abstractmethod
-    def createFriendsIterator(self, profileId):
+    def create_friends_iterator(self, profile_id):
         pass
-
-
-class SocialMedia(SocialNetwork):
-    def __init__(self):
-        self.friendGraph = defaultdict(list)
-        self.profiles = defaultdict(Profile)
-        
-    def setProfile(self, personId, personEmail):
-        if personId not in self.profiles:
-            self.profiles[personId] = Profile(personId, personEmail)
-            
-    def setFriend(self, personId, hisFriendId):
-        if personId in self.profiles and hisFriendId in self.profiles:
-            self.friendGraph[personId].append(self.profiles[hisFriendId])
-    
-    def socialListRequest(self, profileId):
-        return self.friendGraph[profileId] if profileId in self.friendGraph else []
-    
-    def createFriendsIterator(self, profileId):
-        return SocialMediaIterator(self, profileId)
-
 
 class ProfileIterator(ABC):
-    @abstractmethod
-    def getNext(self):
-        pass
+    """Abstract base class for profile iterators."""
+    def __iter__(self):
+        return self
     
     @abstractmethod
-    def hasMore(self):
+    def __next__(self):
         pass
 
+# ---------- Concrete Collection and Iterator ----------
 
-class SocialMediaIterator(ProfileIterator):
-    def __init__(self, socialMedia: SocialMedia, profileId):
-        self.socialMedia = socialMedia
-        self.profileId = profileId
-        self.currentPosition = 0
-        self.cache = []
-        self.initCache()
-        
-    def initCache(self):
-        self.cache = self.socialMedia.socialListRequest(self.profileId)
-        
-    def getNext(self): # changes based on how data structure looks like
-        if self.hasMore():
-            result = self.cache[self.currentPosition]
-            self.currentPosition += 1
-            return result
-        return None
-    
-    def hasMore(self):
-        return self.currentPosition < len(self.cache)
+class SocialMedia(SocialNetwork):
+    """Concrete social network class."""
+    def __init__(self):
+        self.friend_graph = defaultdict(list)  # Adjacency list representation
+        self.profiles = {}  # Dictionary to store profiles by ID
 
+    def add_profile(self, profile_id, email):
+        """Adds a new profile to the social network."""
+        if profile_id not in self.profiles:
+            self.profiles[profile_id] = Profile(profile_id, email)
+
+    def add_friendship(self, profile_id, friend_id):
+        """Creates a bidirectional friendship between two profiles."""
+        if profile_id in self.profiles and friend_id in self.profiles:
+            self.friend_graph[profile_id].append(self.profiles[friend_id])
+            self.friend_graph[friend_id].append(self.profiles[profile_id])
+
+    def get_friends(self, profile_id):
+        """Returns a list of friends for the given profile ID."""
+        return self.friend_graph.get(profile_id, [])
+
+    def create_friends_iterator(self, profile_id):
+        """Creates an iterator for the friends of a given profile."""
+        return FriendIterator(self, profile_id)
+
+class FriendIterator(ProfileIterator):
+    """Iterator for traversing friends of a profile."""
+    def __init__(self, social_media, profile_id):
+        self.social_media = social_media
+        self.profile_id = profile_id
+        self.current_index = 0
+        self.friends = self.social_media.get_friends(self.profile_id)
+
+    def __next__(self):
+        """Returns the next friend in the iteration."""
+        if self.current_index < len(self.friends):
+            friend = self.friends[self.current_index]
+            self.current_index += 1
+            return friend
+        else:
+            raise StopIteration()
+
+# ---------- Profile Class ----------
 
 class Profile:
-    def __init__(self, id, email):
-        self.id = id
+    """Class representing a user profile."""
+    def __init__(self, profile_id, email):
+        self.profile_id = profile_id
         self.email = email
 
-    def getId(self):
-        return self.id
-    
-    def getEmail(self):
+    def get_id(self):
+        """Returns the profile ID."""
+        return self.profile_id
+
+    def get_email(self):
+        """Returns the email of the profile."""
         return self.email
 
+# ---------- Client Code ----------
 
-'''
-    Client area will start from here
-'''
-class SocialGetter: 
-    def get(self, iterator: SocialMediaIterator) -> list:
-        info_list = []
-        while iterator.hasMore():
-            profile = iterator.getNext()
-            info_list.append((profile.getId(), profile.getEmail()))
-        
-        return info_list
+class SocialSpammer:
+    """Client class that uses an iterator to send messages."""
+    def send(self, iterator, message):
+        """Sends a message to all profiles in the iterator."""
+        for profile in iterator:
+            self.send_email(profile.get_email(), message)
 
+    def send_email(self, email, message):
+        """Simulates sending an email (for demonstration purposes)."""
+        print(f"Sending email to {email}: {message}")
+
+# ---------- Application ----------
 
 class Application:
+    """Application that configures and uses the social network."""
     def __init__(self):
-        # to request the data structure from the social network
-        self.getter = SocialGetter()
         self.network = SocialMedia()
-    
-    def getFriendInfo(self, profile: Profile):
-        iterator = self.network.createFriendsIterator(profile.getId())
-        return self.getter.get(iterator)
+        self.spammer = SocialSpammer()
+        self.setup_network()
 
+    def setup_network(self):
+        """Sets up the social network with profiles and friendships."""
+        # Adding profiles
+        self.network.add_profile("1", "user1@example.com")
+        self.network.add_profile("2", "user2@example.com")
+        self.network.add_profile("3", "user3@example.com")
+
+        # Establishing friendships
+        self.network.add_friendship("1", "2")
+        self.network.add_friendship("1", "3")
+
+    def send_spam_to_friends(self, profile_id):
+        """Sends spam messages to friends of the given profile."""
+        iterator = self.network.create_friends_iterator(profile_id)
+        self.spammer.send(iterator, "Buy our new product!")
+
+# ---------- Usage Example ----------
 
 if __name__ == "__main__":
-# Example usage
-    # start of the application -> calls the application interface
     app = Application()
-    app.network.setProfile("1", "profile1@example.com")
-    app.network.setProfile("2", "profile2@example.com")
-    app.network.setProfile("3", "profile3@example.com")
-    app.network.setFriend("1", "2")  # Adding friend relationship
-    app.network.setFriend("1", "3")  # Adding another friend
-
-    profile1 = app.network.profiles["1"]
-
-    print(app.getFriendInfo(profile1))  # Should return [('2', 'profile2@example.com'), ('3', 'profile3@example.com')]
+    app.send_spam_to_friends("1")  # Sends spam to user 1's friends
